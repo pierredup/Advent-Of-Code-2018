02d8914a728b9828c199753ddbe68147019d7477
Pierre du Plessis <pierre@pcservice.co.za>
2018-12-14 08:46:53 +0200

    Add Day 5 - Part 2

--- src/day5/part2.php
+++ src/day5/part2.php
@@ -1,3 +1,28 @@
 <?php
 
-return null;
+function polymer($chars)
+{
+    for ($i = 0; $i <= $c = strlen($chars); $i++) {
+        if (isset($chars[$i + 1]) && 32 === abs(ord($chars[$i]) - ord($chars[$i + 1]))) {
+            $chars = substr_replace($chars, '', $i, 2);
+            $i -= 2;
+
+            if ($i < -1) {
+                $i = 0;
+            }
+        }
+    }
+
+    return $chars;
+}
+
+$min = strlen($input);
+foreach (range('a', 'z') as $char) {
+    $string = str_replace([$char, strtoupper($char)], '', $input);
+
+    if (($length = strlen(polymer($string))) < $min) {
+        $min = $length;
+    }
+}
+
+return $min;
